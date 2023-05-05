f32111e795c33f54771ab2160e4cd9ccb0d767c1
iolowookere217 <olowookere.isaaco@gmail.com>
2023-05-05 12:35:29 +0000

    Adv task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#SSH client configuration
+exec { 'No passwd auth':
+command => 'bash -c "echo PasswordAuthentication no >> /etc/ssh/ssh_config"',
+path    => '/usr/bin:/usr/sbin:/bin'
+}
+exec { 'Identity file':
+command => 'bash -c "echo IdentityFile \'~/.ssh/school\' >> /etc/ssh/ssh_config"',
+path    => '/usr/bin:/usr/sbin:/bin'
+}
+exec { 'Pubkey auth':
+command => 'bash -c "echo PubkeyAuthentication yes >> /etc/ssh/ssh_config"',
+path    => '/usr/bin:/usr/sbin:/bin
+}
