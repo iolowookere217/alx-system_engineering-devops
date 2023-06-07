bb6f943d866fb25a570fd6f1d0aa35f93c3a369e
iolowookere217 <olowookere.isaaco@gmail.com>
2023-06-07 19:05:00 +0000

    Correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # A puppet file that automatically fix error 500 returned by Apache 
 file { '/var/www/html/wp-settings.php':
   ensure => present,
-  content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub("phpp", "php") %>'),
+  content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub(/\.phpp/, ".php") %>'),
 }
