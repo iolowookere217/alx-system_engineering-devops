3078fb5f7d2327b3b93cd5f0991f7304af10baa1
iolowookere217 <olowookere.isaaco@gmail.com>
2023-06-07 19:29:44 +0000

    Puppet link check

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# A puppet file that automatically fix error 500 returned by Apache 
+# A puppet file that automatically fix error 500 returned by Apache
+
 file { '/var/www/html/wp-settings.php':
-  ensure => present,
+  ensure  => present,
   content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub(/\.phpp/, ".php") %>'),
 }
