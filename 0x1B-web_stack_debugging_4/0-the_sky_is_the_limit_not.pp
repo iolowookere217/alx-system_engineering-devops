ec52ae2919391ac5b8a1e70ae9df3cb7735f4bf6
iolowookere217 <olowookere.isaaco@gmail.com>
2023-06-16 12:07:28 +0000

    2048_task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # A puppet manifest that increases Ulimit to accommodate large requests
 file { '/etc/default/nginx':
   ensure  => file,
-  content => "ULIMIT='-n 2000'\nCONCURRENCY=100\n",
+  content => "ULIMIT='-n 2048'\nCONCURRENCY=100\n",
 }
