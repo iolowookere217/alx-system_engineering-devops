0b739d84efbfa5fa287b757c36173c06bfc909d6
iolowookere217 <olowookere.isaaco@gmail.com>
2023-06-16 11:59:18 +0000

    latest update task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # A puppet manifest that increases Ulimit to accommodate large requests
 file { '/etc/default/nginx':
   ensure  => file,
-  content => "ULIMIT='-n 4096'\n",
+  content => "ULIMIT='N 2048'\nCONCURRENCY=100\n",
 }
